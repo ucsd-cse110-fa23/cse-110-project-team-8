5e12562ee5125d73a88c9cc9d58b012c99488258
kaijia2022 <102701288+kaijia2022@users.noreply.github.com>
2023-11-01 08:45:31 +0800

    Created project Template
    Created Classes Recipe, RecipeList, AppFrame and App

---
+++ src/AppFrame.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+import java.io.*;
+import java.util.*;
+
+public class AppFrame {
+    
+}
---
+++ src/RecipeList.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class RecipeList {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
