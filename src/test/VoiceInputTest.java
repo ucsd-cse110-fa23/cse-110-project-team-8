c98c7524b80a32be9661d31e69b8ff6a222c94ff
333GUSSS <gbaez@ucsd.edu>
2023-11-05 20:35:15 -0800

    Created "testGPT()"

--- src/test/VoiceInputTest.java
+++ src/test/VoiceInputTest.java
@@ -5,26 +5,40 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import VoiceInput.*;
 import PantryPal.*;
 
 public class VoiceInputTest {
-    private Recipe recipe;
+    //private Recipe recipe;
+    private ChatGPT chatGPT;
 
     @BeforeEach
     void setUp() {
-        recipe = new Recipe();
-        recipe.FILE_PATH = "test1.wav";
-    }
-
-    @Test
-    public void testRecipe() {
+        //recipe = new Recipe();
+        chatGPT = new ChatGPT();
+        //recipe.FILE_PATH = "test1.wav";
 
     }
 
     @Test
-    public void testRecipe2() {
+    public void testGPT() {
+        String testGPTinput = "I want you to give print the numbers '123' exactly like that, do not type anything else.";
+
+        String testGPToutput;
+        
+        try {
+            testGPToutput = chatGPT.chefGPT(testGPTinput);
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            e.printStackTrace();
+            testGPToutput = "Error";
+        }
+
+        assertEquals("\n" + "\n" + "123", testGPToutput);
     }
 }
