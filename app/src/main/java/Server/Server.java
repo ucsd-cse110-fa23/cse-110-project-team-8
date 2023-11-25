55b94b00f9e647857a6b46bd0b3d6a60edc803a5
Henryfzh <henryfzh@outlook.com>
2023-11-25 14:11:42 -0800

    #72 #73

--- app/src/main/java/Server/Server.java
+++ app/src/main/java/Server/Server.java
@@ -43,6 +43,7 @@ public class Server {
         0);
     this.server = server;
 
+    server.createContext("/", new RequestHandler(mongoClient));
     // set the executor
     server.setExecutor(threadPoolExecutor);
 
